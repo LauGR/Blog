7b58598f66c7fe8685b79f5294ac08a3b1ddc194
smile <smile@localhost.localdomain>
2017-12-04 11:52:43 +0400

    admin de merde

--- app/routes/panelAdmin.js
+++ app/routes/panelAdmin.js
@@ -2,121 +2,123 @@
     const multer = require('multer');
     const fs = require('fs');
     const article = require('../models/article')
+    const user = require('../models/user')
     const upload = multer({
         dest: 'public/images/'
     })
 
     module.exports = (app, passport) => {
 
-        // PROFIL ADMIN 
-
-        app.get('/dashbord/profil',permissions.can('access admin page'), (req, res) => {
-            res.render('profil',{layout:'layoutAdmin'})
-        })
 
 
-        // PANEL ADMIN 
+            // PANEL ADMIN 
 
-        app.get('/dashbord', permissions.can('access admin page'), (req, res) => {
-            article.find((err, article) => {
-                res.render('dashbord', {
-                    articles: article,
-                    layout: 'layoutAdmin'
+            app.get('/dashbord', permissions.can('access admin page'), (req, res) => {
+                article.find((err, article) => {
+                    res.render('dashbord', {
+                        article: article,
+                        layout: 'layoutAdmin'
+                    })
+
                 })
-
-            })
-        });
-
-        app.get('/article/:id/delete', permissions.can('access admin page'), (req, res) => {
-            article.remove({
-                _id: req.params.id
-            }, (err, delData) => {
-                res.redirect("/dashbord");
-            })
-        })
-
-        app.get('/dashbord/articles', permissions.can('access admin page'), (req, res) => {
-            res.render('createarticle', {
-                layout: 'layoutAdmin'
-            });
-        });
-
-
-        // CREATE ARTICLE PANEL ADMIN
-
-
-        app.post('/dashbord/articles', permissions.can('access admin page'), (req, res) => {
-
-            let myData = new article({
-                name: req.body.name,
-                url: req.body.url,
-                dateA: req.body.dateA,
-                dateR: req.body.dateR,
-                sejour: req.body.sejour,
-                preview: req.body.preview,
-
             });
 
-            myData
-                .save()
-                .then(item => {
-                    //delete temp file
-                    fs.unlink(tmp_path);
-                    src.on('end', () => {
-                        res.redirect("/dashbord/articles");
-                    });
-                    src.on('error', (err) => {
-                        res.render('error');
-                    });
-
-                })
-                .catch(err => {
-                    res
-                        .status(400)
-                });
-        });
-        // UPDATE article PANEL ADMIN
-
-        app.get('/updatearticle/:id', permissions.can('access admin page'), (req, res) => {
-            article.find((err, articles) => {
-                res.render('updatearticle', {
-                    layout: 'layoutAdmin',
-                    article: req.params.id,
-                    article: articles.filter((article) => {
-                        return article.id == req.params.id
-                    })[0]
+            app.get('/article/:id/delete', permissions.can('access admin page'), (req, res) => {
+                article.remove({
+                    _id: req.params.id
+                }, (err, delData) => {
+                    res.redirect("/dashbord");
                 })
             })
-        })
 
-        app.post('/updatearticle/:id', permissions.can('access admin page'), (req, res) => {
+            app.get('/dashbord/articles', permissions.can('access admin page'), (req, res) => {
+                        article.find((err, article) => {
+                            res.render('createarticle', {
+                                article: article,
+                                layout: 'layoutAdmin'
+                            });
+                        });
+                    })
 
-                article.findByIdAndUpdate(req.params.id, {
-                        $set: {
-                            name: req.body.name,
-                            url: req.body.url,
-                            dateA: req.body.dateA,
-                            dateR: req.body.dateR,
-                            sejour: req.body.sejour,
-                            preview: req.body.preview,
-                            text: req.body.text,
-                            img: img_path
-                        }
-                    }, {
-                        new: true
-                    }, (err, article) => {
-                        article.save().then(item => {
-                                //delete temp file
-                                fs.unlink(tmp_path);
-                                console.log('Ca marche toujours')
-                            },
-                            res.redirect('/dashbord')
-                        )
-                    .catch(err => {
-                        res.status(400);
-                    });
 
-                })
-        })
+                        // PROFIL ADMIN 
 
-    }
+                        app.get('/dashbord/profil', permissions.can('access admin page'), (req, res) => {
+                            user.find((err, user) => {
+                                res.render('profil', {
+                                    user: user,
+                                    layout: 'layoutAdmin'
+                                })
+
+                            })
+                        })
+
+                        // CREATE ARTICLE PANEL ADMIN
+
+
+                        app.post('/dashbord/articles', permissions.can('access admin page'), (req, res) => {
+
+                            let myData = new article({
+                                title: req.body.title,
+                                date: req.body.date,
+                                content: req.body.content
+                            });
+
+                            myData
+                                .save()
+                                .then(item => {
+                                    //delete temp file
+                                    fs.unlink(tmp_path);
+                                    src.on('end', () => {
+                                        res.redirect("/dashbord");
+                                    });
+                                    src.on('error', (err) => {
+                                        res.render('error');
+                                    });
+
+                                })
+                                .catch(err => {
+                                    res
+                                        .status(400)
+                                });
+                        });
+                        // UPDATE article PANEL ADMIN
+
+                        app.get('/updatearticle/:id', permissions.can('access admin page'), (req, res) => {
+                            article.find((err, articles) => {
+                                res.render('updatearticle', {
+                                    layout: 'layoutAdmin',
+                                    article: req.params.id,
+                                    article: articles.filter((article) => {
+                                        return article.id == req.params.id
+                                    })[0]
+                                })
+                            })
+                        })
+
+                        app.post('/updatearticle/:id', permissions.can('access admin page'), (req, res) => {
+
+                            article.findByIdAndUpdate(req.params.id, {
+                                $set: {
+                                    title: req.body.title,
+                                    date: req.body.date,
+                                    content: req.body.content
+                                }
+                            }, {
+                                new: true
+                            }, (err, article) => {
+                                article.save().then(item => {
+                                            //delete temp file
+                                            fs.unlink(tmp_path);
+                                            console.log('Ca marche toujours')
+                                        },
+                                        res.redirect('/dashbord')
+                                    )
+                                    .catch(err => {
+                                        res.status(400);
+                                    });
+
+                            })
+                        })
+
+                    }
