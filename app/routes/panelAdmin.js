e6355ea0fd1b21ec04c22ec1ef07552c4a9ca7dd
LauGR <laurent.gregoire974@gmail.com>
2017-12-08 11:48:27 +0400

    code review ok

--- app/routes/panelAdmin.js
+++ app/routes/panelAdmin.js
@@ -75,7 +75,6 @@ module.exports = (app, passport) => {
     })
 
     // change password 
-
     app.post("/dashboard/changepassword", permissions.can('access admin page'), (req, res) => {
         // hash password before update
         req.body.password = bcrypt.hashSync(req.body.password);
@@ -94,7 +93,7 @@ module.exports = (app, passport) => {
 
 
 
-    // PANEL ADMIN 
+    // LIST ARTICLES
     app.get('/dashboard', permissions.can('access admin page'), (req, res) => {
         article.find((err, article) => {
             res.render('dashboard', {
@@ -105,9 +104,8 @@ module.exports = (app, passport) => {
     });
 
     // CREATE  DRAFT COPY PANEL ADMIN 
-
     app.get('/dashboard/createarticle', permissions.can('access admin page'), (req, res) => {
-        res.render('createarticle', {
+        res.render('createArticle', {
             layout: 'layoutAdmin'
         });
     });
@@ -144,131 +142,6 @@ module.exports = (app, passport) => {
                     .status(400)
             });
     });
-
-    // LIST ARTICLE PANEL ADMIN
-
-    app.get('/dashboard/listdraftcopy', permissions.can('access admin page'), (req, res) => {
-        article.find({}, function (err, article) {
-            res.render('listdraftcopy.ejs', {
-                article: article,
-                layout: 'layoutAdmin'
-            })
-        })
-
-    })
-
-
-    app.post('/dashboard/listdraftcopypush/:id', permissions.can('access admin page'), upload.single('img'), (req, res) => {
-        // Create let for img
-        let fileToUpload = req.file;
-        let target_path;
-        let tmp_path;
-        let img_path;
-        if (fileToUpload != undefined || fileToUpload != null) {
-            console.log('file est defini')
-            target_path = 'public/images/' + fileToUpload.originalname;
-            tmp_path = fileToUpload.path;
-            img_path = fileToUpload.originalname;
-        } else {
-            console.log('pas ok')
-            img_path = req.body.img;
-        }
-        article.findByIdAndUpdate(req.params.id, {
-            $set: {
-                title: req.body.title,
-                date: req.body.date,
-                content: req.body.content,
-                img: img_path,
-                brouillon: false
-            }
-        }, {
-            new: true
-        }, (err, article) => {
-            article.save().then(item => {
-                    // console.log('Ca marche')
-                    if (fileToUpload != undefined || fileToUpload != null) {
-                        let src = fs.createReadStream(tmp_path);
-                        let dest = fs.createWriteStream(target_path);
-                        src.pipe(dest);
-                        //delete temp file
-                        fs.unlink(tmp_path);
-                        console.log('Ca marche toujours')
-                    }
-                    res.redirect('/dashboard/listdraftcopy')
-                })
-                .catch(err => {
-                    res.status(400);
-                });
-
-        })
-    })
-
-    // SAVE DRAFT COPY 
-
-    app.get('/dashboard/listdraftcopyupdate/:id', permissions.can('access admin page'), (req, res) => {
-        article.find((err, article) => {
-            res.render('listdraftcopyUpdate', {
-                layout: 'layoutAdmin',
-                article: req.params.id,
-                article: article.filter((article) => {
-                    return article.id == req.params.id
-                })[0]
-            })
-        })
-    })
-
-    app.post('/dashboard/listdraftcopyupdate/:id', permissions.can('access admin page'), upload.single('img'), (req, res) => {
-        // Create let for img
-        let fileToUpload = req.file;
-        let target_path;
-        let tmp_path;
-        let img_path;
-        if (fileToUpload != undefined || fileToUpload != null) {
-            console.log('file est defini')
-            target_path = 'public/images/' + fileToUpload.originalname;
-            tmp_path = fileToUpload.path;
-            img_path = fileToUpload.originalname;
-        } else {
-            console.log('pas ok')
-            img_path = req.body.img;
-        }
-        article.findByIdAndUpdate(req.params.id, {
-            $set: {
-                title: req.body.title,
-                date: req.body.date,
-                content: req.body.content,
-                img: img_path
-            }
-        }, {
-            new: true
-        }, (err, article) => {
-            article.save().then(item => {
-                    // console.log('Ca marche')
-                    if (fileToUpload != undefined || fileToUpload != null) {
-                        let src = fs.createReadStream(tmp_path);
-                        let dest = fs.createWriteStream(target_path);
-                        src.pipe(dest);
-                        //delete temp file
-                        fs.unlink(tmp_path);
-                        console.log('Ca marche toujours')
-                    }
-                    res.redirect('/dashboard/listdraftcopy')
-                })
-                .catch(err => {
-                    res.status(400);
-                });
-        })
-    })
-
-
-    //REMOVE DRAFT COPY ARTICLE 
-    app.get('/dashboard/listdraftcopysupp/:id/delete', permissions.can('access admin page'), (req, res) => {
-        article.remove({
-            _id: req.params.id
-        }, (err, delData) => {
-            res.redirect("/dashboard/listdraftcopy");
-        })
-    })
 
     // CREATE ARTICLE PANEL ADMIN
     app.post('/dashboard/createarticle', permissions.can('access admin page'), upload.single('img'), (req, res) => {
@@ -362,9 +235,8 @@ module.exports = (app, passport) => {
         })
     })
 
-    // DELETE ARTICLE PANEL ADMIN
-
-    app.get('/dashboard/article/:id/delete', permissions.can('access admin page'), (req, res) => {
+       // DELETE ARTICLE PANEL ADMIN
+       app.get('/dashboard/article/:id/delete', permissions.can('access admin page'), (req, res) => {
         article.remove({
             _id: req.params.id
         }, (err, delData) => {
@@ -372,4 +244,132 @@ module.exports = (app, passport) => {
         })
     })
 
+
+
+    // LIST DRAFT COPY PANEL ADMIN
+    app.get('/dashboard/listdraftcopy', permissions.can('access admin page'), (req, res) => {
+        article.find({}, function (err, article) {
+            res.render('listdraftCopy.ejs', {
+                article: article,
+                layout: 'layoutAdmin'
+            })
+        })
+
+    })
+
+    // PUBLISH DRAFT COPY ON LIST ARTICLES
+    app.post('/dashboard/listdraftcopypush/:id', permissions.can('access admin page'), upload.single('img'), (req, res) => {
+        // Create let for img
+        let fileToUpload = req.file;
+        let target_path;
+        let tmp_path;
+        let img_path;
+        if (fileToUpload != undefined || fileToUpload != null) {
+            console.log('file est defini')
+            target_path = 'public/images/' + fileToUpload.originalname;
+            tmp_path = fileToUpload.path;
+            img_path = fileToUpload.originalname;
+        } else {
+            console.log('pas ok')
+            img_path = req.body.img;
+        }
+        article.findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                date: req.body.date,
+                content: req.body.content,
+                img: img_path,
+                brouillon: false
+            }
+        }, {
+            new: true
+        }, (err, article) => {
+            article.save().then(item => {
+                    // console.log('Ca marche')
+                    if (fileToUpload != undefined || fileToUpload != null) {
+                        let src = fs.createReadStream(tmp_path);
+                        let dest = fs.createWriteStream(target_path);
+                        src.pipe(dest);
+                        //delete temp file
+                        fs.unlink(tmp_path);
+                        console.log('Ca marche toujours')
+                    }
+                    res.redirect('/dashboard/listdraftcopy')
+                })
+                .catch(err => {
+                    res.status(400);
+                });
+
+        })
+    })
+
+    // SAVE MODIFICATIONS DRAFT COPY 
+    app.get('/dashboard/listdraftcopyupdate/:id', permissions.can('access admin page'), (req, res) => {
+        article.find((err, article) => {
+            res.render('listdraftcopyUpdate', {
+                layout: 'layoutAdmin',
+                article: req.params.id,
+                article: article.filter((article) => {
+                    return article.id == req.params.id
+                })[0]
+            })
+        })
+    })
+
+    app.post('/dashboard/listdraftcopyupdate/:id', permissions.can('access admin page'), upload.single('img'), (req, res) => {
+        // Create let for img
+        let fileToUpload = req.file;
+        let target_path;
+        let tmp_path;
+        let img_path;
+        if (fileToUpload != undefined || fileToUpload != null) {
+            console.log('file est defini')
+            target_path = 'public/images/' + fileToUpload.originalname;
+            tmp_path = fileToUpload.path;
+            img_path = fileToUpload.originalname;
+        } else {
+            console.log('pas ok')
+            img_path = req.body.img;
+        }
+        article.findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                date: req.body.date,
+                content: req.body.content,
+                img: img_path
+            }
+        }, {
+            new: true
+        }, (err, article) => {
+            article.save().then(item => {
+                    // console.log('Ca marche')
+                    if (fileToUpload != undefined || fileToUpload != null) {
+                        let src = fs.createReadStream(tmp_path);
+                        let dest = fs.createWriteStream(target_path);
+                        src.pipe(dest);
+                        //delete temp file
+                        fs.unlink(tmp_path);
+                        console.log('Ca marche toujours')
+                    }
+                    res.redirect('/dashboard/listdraftcopy')
+                })
+                .catch(err => {
+                    res.status(400);
+                });
+        })
+    })
+
+
+    //REMOVE DRAFT COPY 
+    app.get('/dashboard/listdraftcopysupp/:id/delete', permissions.can('access admin page'), (req, res) => {
+        article.remove({
+            _id: req.params.id
+        }, (err, delData) => {
+            res.redirect("/dashboard/listdraftcopy");
+        })
+    })
+
+    
 }
+    
+ 
